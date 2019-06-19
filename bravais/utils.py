bcb81863446420204f2b3a1ad89c70f7f0650699
aplowman <adam.jp@live.co.uk>
2019-06-19 21:11:32 +0100

    Add Sites and SitesLabel classes

---
+++ bravais/utils.py
@@ -0,0 +1,26 @@
+"""bravais.utils.py`
+
+Utility functions.
+
+"""
+
+
+def check_indices(seq, seq_idx):
+    """
+    Given a sequence (e.g. list, tuple, ndarray) which is indexed by another,
+    check the indices are sensible.
+
+    Parameters
+    ----------
+    seq : sequence
+    seq_idx : sequence of int
+
+    """
+
+    # Check: minimum index is greater than zero
+    if min(seq_idx) < 0:
+        raise IndexError('Found index < 0.')
+
+    # Check maximum index is equal to length of sequence - 1
+    if max(seq_idx) > len(seq) - 1:
+        raise IndexError('Found index larger than seqence length.')
